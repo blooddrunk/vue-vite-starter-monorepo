402c04d636764fe42879c39e4409866e2cefe23b
Xie Yuhao <jelinenaro@gmail.com>
2023-02-06 17:44:48 +0800

    WIP

---
+++ .prettierrc.js
@@ -0,0 +1,28 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    // 'prettier-plugin-tailwindcss',
+  ],
+  pluginSearchDirs: false,
+
+  // options of @ianvs/prettier-plugin-sort-imports
+  // '^@/typings(.*)$',
+  importOrder: [
+    '<TYPES>',
+    '<TYPES>^[./]',
+    '<THIRD_PARTY_MODULES>',
+    '(.*)\\.(vue|tsx)$',
+    '^@/(.*)$',
+    '^[./]',
+  ],
+  importOrderBuiltinModulesToTop: true,
+  importOrderCaseInsensitive: true,
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
