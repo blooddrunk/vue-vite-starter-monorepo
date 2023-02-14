ec6df004419118366a60207e518beeaf9bedfa0b
Xie Yuhao <jelinenaro@gmail.com>
2023-02-14 18:41:15 +0800

    still a mess

---
+++ packages/tailwind-config/postcss.config.cjs
@@ -0,0 +1,24 @@
+const config = require('./tailwind.config.cjs');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = () => {
+  return {
+    plugins: {
+      // https://github.com/csstools/postcss-preset-env
+      // defaults to stage 2
+      'postcss-preset-env': {
+        stage: 2,
+        features: { 'nesting-rules': false },
+      },
+
+      // https://github.com/cssnano/cssnano
+      cssnano: isDev ? false : { preset: 'default' },
+
+      'tailwindcss/nesting': {},
+
+      // https://github.com/tailwindcss/tailwindcss
+      tailwindcss: { config },
+    },
+  };
+};
